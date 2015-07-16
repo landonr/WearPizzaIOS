21a793f481615105c0fc20be9362e01e141781a5
Kale Baiton <kaleb@iqmetrix.com>
2015-07-15 19:03:57 -0500

    Initial Commit of Wear Pizza IOS

---
+++ WearPizzaIOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WearPizzaIOS
+//
+//  Created by Kale Baiton on 2015-07-15.
+//  Copyright (c) 2015 Kale Baiton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
